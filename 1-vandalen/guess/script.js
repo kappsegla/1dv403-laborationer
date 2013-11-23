29572bd83733c2201ac332a7ad9265d0fe919632
Martin Blomberg <martin.blomberg@lnu.se>
2013-11-23 18:19:19 +0100

    Uppgift1 - Gissa hemliga talet implementerad.

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,21 +2,43 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+	var secret = Math.floor( Math.random() * 100)+1; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var numberOfGuesses = 0;
+
+	var errorMessages = ["\"The fact that man knows right from wrong proves his intellectual superiority to the other creatures; but the fact that he can do wrong proves his moral inferiority to any creatures that cannot.\" - Mark Twain", "\"Wrong does not cease to be wrong because the majority share in it.\" - Leo Tolstoy","\"Gossip, as usual, was one-third right and two-thirds wrong.\" - L.M. Montgomery"]
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
+		
+		numberOfGuesses++;
+ 
+
 		// Plats för förändring.
+		if( number < 1 || number > 100) {
+			return [false, "Talet är utanför intervallet 1 - 100"];
+		}	
+
+		if( number == secret) {
+			return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + numberOfGuesses + " gissningar för att hitta det."];
+		}
+		else if( number < secret) {
+			return [false, "Det hemliga talet är högre!"];
+		}
+		else if( number > secret) {
+			return [false, "Det hemliga talet är lägre!"];
+		}
+		else {
+			return [false, errorMessages[ (numberOfGuesses + 1) % 3]];
+
+		}
 
 
-		// Returnera exempelvis: 
+		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
 		// [false, "Det hemliga talet är högre!"]
 		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+		// [false, "Talet är utanför intervallet 1 - 100"]		
 	};
 	
 	// ------------------------------------------------------------------------------
