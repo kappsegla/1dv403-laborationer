f2381bae022da2e6b9bd1733103811559acd7c32
Martin Blomberg <martin.blomberg@lnu.se>
2013-11-23 18:39:22 +0100

    String upper/lowercase implemented.

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -8,8 +8,28 @@ window.onload = function(){
 		// Returnera den konverterade strängen.
 		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
 	
+		if(  typeof str != 'string' || str.length < 1 ) {
+			throw new Error("Not a string or too few characters detected."); 
+		}	 
+		
+		var modified_str = "";
 
+		for (var i = 0, max = str.length; i < max; i++) {
+           var c = str.charAt(i);
+           if( c == 'a' || c == 'A') {
+        		c = '#';
+           }
+           else if(c.toUpperCase() != c) {
+           	c =	c.toUpperCase();
+           }
+           else if(c.toLowerCase() != c) {
+           		c = c.toLowerCase();
+           }
 
+         	modified_str += c;
+        }
+
+		return modified_str;
 
 
 
